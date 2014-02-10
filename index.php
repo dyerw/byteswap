b911aabfd7d879bc219ccbaad5e8dd0354f38942
dyerw <dyer.w@husky.neu.edu>
2014-02-09 19:25:45 -0600

    downloads from S3 now have correct content-type, messages about uploads stored on server

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
     <div id="main">
         <p>
             Files are capped at 1MB for now. Thanks for trying this out.
-            Currently in transition and acting wooooonky.
         </p>
         <p>
             Here's what to do: 1. upload a file 2. get a random file someone else uploaded
         </p>
         <form action="upload_file.php" method="post" enctype="multipart/form-data">
             <input type="file" name="upload_file" id="upload_field"><br>
+            <label for="msg_field">What is this?</label>
+            <input type="text" name="upload_msg" id="msg_field"><br>
             <input type="submit" name="submit" value="Upload File">
         </form>
         <button onclick="loadBucketContents()">What's in the file hotel?</button>
--- upload_file.php
+++ upload_file.php
@@ -11,6 +11,8 @@ $client = Aws\S3\S3Client::factory(array(
     'key' => $aws_key,
     'secret' => $aws_secret_key
 ));
+//gets the message the user uploaded with the file
+$usr_msg = $_POST["upload_msg"];
 
 // gets the files size in MB
 $file_size = $_FILES["upload_file"]["size"] / 1024 /1024;
@@ -29,15 +31,24 @@ if ($_FILES['upload_file']['error'] > 0) {
     $file_name = $_FILES['upload_file']['name'];
     $file_type = $_FILES['upload_file']['type'];
     $tmp_file_path = $_FILES['upload_file']['tmp_name'];
-    $new_name = uniqid(rand(), true); // Currently not renaming files
+    $unique_id = uniqid();
 
     //Put file into S3 bucket
     $client->putObject(array(
         'Bucket' => 'FileHotel',
         // TODO: prepend unique id to key to prevent overwrites
-        'Key' => $file_name,
-        'SourceFile' => $tmp_file_path
+        'Key' => $unique_id.".".$file_name,
+        'SourceFile' => $tmp_file_path,
+        'ContentType' => $file_type
     ));
+
+    //Store the user message in the csv file
+    file_put_contents("./messages.csv", "\n".$unique_id.",".$usr_msg, $flag=FILE_APPEND);
+
+    /*
+     * This downloads a file randomly from the S3 bucket
+     * TODO: this should probably be separate from the upload
+     */
 
     // get an iterator for all the objects in the bucket
     $iterator = $client->getIterator('ListObjects', array(
@@ -53,15 +64,20 @@ if ($_FILES['upload_file']['error'] > 0) {
     $rand_index = rand(0, count($keys_list) - 1);
     $key = $keys_list[$rand_index];
 
-    $signedUrl = $client->getObjectUrl('FileHotel', $key, '+10 minutes');
-
-    echo $signedUrl;
+    $signedUrl = $client->getObjectUrl('FileHotel', $key, '+1 minutes');
+    // get content type
+    $head_array = $client->headObject(array(
+        'Bucket' => 'FileHotel',
+        'Key' => $key
+    ));
+    $download_type = $head_array['ContentType'];
 
     //Download a file for the user
     header("Content-Transfer-Encoding: Binary");
-    header("Content-disposition: attachment; filename=\"" . $key . "\"");
+    $real_file_name =  explode(".", $key); //strip the unique id
+    header("Content-Type: ".$download_type);
+    header("Content-disposition: attachment; filename=\"" . $real_file_name[1] . "\"");
     ob_clean();
     flush();
     readfile($signedUrl);
-
 }
