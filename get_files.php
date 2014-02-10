7be6d1c7a818b3b7aa758150d9dff6114f232332
dyerw <dyer.w@husky.neu.edu>
2014-02-09 20:17:31 -0600

    file listing now shows user message

--- get_files.php
+++ get_files.php
@@ -15,11 +15,24 @@ $client = Aws\S3\S3Client::factory(array(
 $iterator = $client->getIterator('ListObjects', array(
     'Bucket' => 'FileHotel'
 ));
+$user_messages = file_get_contents("./messages.csv");
+$msg_lines = explode("\n", $user_messages);
+$msg_array = array();
+foreach ($msg_lines as $line) {
+    $tmp = explode(",", $line);
+    // msg_array[unique_id] = message
+    $msg_array[$tmp[0]] = $tmp[1];
+}
 
 $color_class = array("class=\"red\"","class=\"yellow\"","class=\"blue\"");
 $ind = 0;
 foreach ($iterator as $object) {
-    echo "<p " . $color_class[$ind] . ">" . $object['Key'] . "</p>";
+    $tmp = explode(".", $object['Key']);
+    $unique_id = $tmp[0];
+    $file_name = $tmp[1];
+
+    echo "<p " . $color_class[$ind] . ">"
+        . $file_name . ", " . $msg_array[$unique_id] . "</p>";
     $ind++;
     if ($ind == count($color_class)){
         $ind = 0;
